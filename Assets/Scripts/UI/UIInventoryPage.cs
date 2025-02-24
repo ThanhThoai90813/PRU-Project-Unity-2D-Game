b457c773cc2bb1e347687820344f795ab0b56a3b
thoaidtse <thanhthoai90813@gmail.com>
2025-02-24 18:48:13 +0700

    update Inventory UI

--- Assets/Scripts/UI/UIInventoryPage.cs
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +12,20 @@ public class UIInventoryPage : MonoBehaviour
     [SerializeField]
     private RectTransform contentPanel;
 
+    [SerializeField]
+    private UIInventoryDescription itemDescription;
+
     List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public Sprite image;
+    public int quantity;
+    public string title, description;
+
+    private void Awake()
+    {
+        Hide();
+        itemDescription.ResetDescription();
+    }
 
     public void InitializeInventoryUI(int inventorysize)
     {
@@ -21,11 +35,40 @@ public class UIInventoryPage : MonoBehaviour
                 Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentPanel);
             listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
     }
+
+    private void HandleShowItemActions(UIInventoryItem item)
+    {
+    }
+
+    private void HandleEndDrag(UIInventoryItem item)
+    {
+    }
+    private void HandleSwap(UIInventoryItem item)
+    {
+    }
+    private void HandleBeginDrag(UIInventoryItem item)
+    {
+    }
+
+    private void HandleItemSelection(UIInventoryItem item)
+    {
+        itemDescription.SetDescription(image, title, description);
+        listOfUIItems[0].Select();
+    }
+
     public void Show()
     {
         gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+
+        listOfUIItems[0].SetData(image, quantity);
     }
 
     public void Hide()
